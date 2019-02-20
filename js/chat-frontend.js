29451e76c10701255bbd762538d5b83c3b041466
Md. Hasnain <hasnain@w3engineers.com>
2019-02-20 19:12:50 +0600

    change

--- js/chat-frontend.js
+++ js/chat-frontend.js
@@ -4,9 +4,14 @@ $(function() {
     var content = $('#content')
     var input = $('#input')
     var status = $('#status')
+	
+	var emailEt = $('#email')
+    var passwordEt = $('#password')
+	var loginBtn = $('#login')
 
     var myColor = false
     var myName = false
+	var mySessionId = false
 
     window.WebSocket = window.WebSocket || window.MozWebSocket
 
@@ -20,10 +25,13 @@ $(function() {
         return
     }
 
-    var connection = new WebSocket('ws://192.168.1.104:1337')
+    var connection = new WebSocket('ws://192.168.2.28:1337')
 
     connection.onopen = function() {
         input.removeAttr('disabled')
+        emailEt.removeAttr('disabled')
+        passwordEt.removeAttr('disabled')
+        loginBtn.removeAttr('disabled')
         status.text('Choose name: ')
     }
 
@@ -44,6 +52,10 @@ $(function() {
         }
 
         switch(json.type) {
+			case 'welcome':
+				mySessionId = json.data.session_id
+                break
+				
             case 'color':
                 myColor = json.data
                 status.text(myName + ': ').css('color', myColor)
@@ -51,16 +63,23 @@ $(function() {
                 break
             
             case 'history':
-                for (var i = 0; i < json.data.length; i++) {
-                    addMessage(json.data[i].author, json.data[i].text,
-                        json.data[i].color, new Date(json.data[i].time))
+				var history = json.data
+				console.log(json.data)
+                for (var i = 0; i < history.length; i++) {
+                    addMessage(history[i].name, history[i].message,
+                        'green', new Date(history[i].time))
                 }
                 break
 
             case 'message':
+				alert('new message')
                 input.removeAttr('disabled')
                 addMessage(json.data.author, json.data.text,
                     json.data.color, new Date(json.data.time))
+                break
+				
+			case 'error':
+				alert(json.data.error)
                 break
 
             default:
@@ -74,8 +93,13 @@ $(function() {
             var msg = $(this).val()
             if(!msg) return
 
-            connection.send(msg)
-            console.log('sent to websocket: ' + msg)
+			var obj = {
+				session_id: mySessionId,
+				type: 'broadcast',
+				message: msg
+			}
+            connection.send(JSON.stringify(obj))
+            console.log('sent to websocket: ' + JSON.stringify(obj))
             $(this).val('')
 
             input.attr('disabled', 'disabled')
@@ -85,6 +109,21 @@ $(function() {
             }
         }
     })
+	
+	loginBtn.on('click', () => {
+		var email = emailEt.val()
+		var password = passwordEt.val()
+		
+		var obj = {
+			session_id: mySessionId,
+			type: 'authenticate',
+			email: email,
+			password: password
+		}
+		
+		console.log('sent to websocket: ' + JSON.stringify(obj))
+		connection.send(JSON.stringify(obj))
+	})
 
     setInterval(function() {
         if(connection.readyState !== 1) {
